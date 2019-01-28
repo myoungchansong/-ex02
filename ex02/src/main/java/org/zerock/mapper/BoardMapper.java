79fe308429df6ae7b2dfea71950a69a6b1ef7c81
myoungchansong <audcks68@gmail.com>
2019-01-28 16:29:30 +0900

    게시글 CRUD

---
+++ ex02/src/main/java/org/zerock/mapper/BoardMapper.java
@@ -0,0 +1,27 @@
+package org.zerock.mapper;
+
+import java.util.List;
+
+import org.zerock.domain.BoardVO;
+import org.zerock.domain.Criteria;
+
+public interface BoardMapper {
+	
+	
+	
+	public List<BoardVO> getList();
+	
+	public List<BoardVO> getListWithPaging(Criteria cri);
+	
+	public void insert(BoardVO board);
+	
+	public void insertSelectKey(BoardVO board);
+	
+	public BoardVO read(int bno);
+	
+	public int delete(int bno);
+	
+	public int update(BoardVO board);
+	
+	public int gettotalCount(Criteria cri);
+}
---
+++ ex02/src/test/java/org/zerock/mapper/BoardMapperTests.java
@@ -0,0 +1,121 @@
+package org.zerock.mapper;
+
+import java.util.List;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.zerock.domain.BoardVO;
+import org.zerock.domain.Criteria;
+
+import lombok.Setter;
+import lombok.extern.log4j.Log4j;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("file:src/main/webapp/WEB-INF/spring/root-context.xml")
+@Log4j
+public class BoardMapperTests {
+	@Setter(onMethod_=@Autowired)
+	private BoardMapper mapper;
+	
+/*	@Test
+	public void testGetList() {
+		mapper.getList().forEach(board -> log.info(board));
+	}
+	*/
+	/*@Test
+	public void testInsert() {
+		BoardVO board = new BoardVO();
+		board.setTitle("새로 작성하는 글 제목");
+		board.setContent("새로 작성하는 내용");
+		board.setWriter("Newbie");
+		
+		mapper.insert(board);
+		
+		log.info(board);
+	}*/
+	
+	
+	
+/*	@Test
+	public void testinsertSelectKey() {
+		BoardVO board = new BoardVO();
+		board.setTitle("새로 작성하는 글 select key");
+		board.setContent("새로 작성하는 내용 select key");
+		board.setWriter("newbie");
+
+		mapper.insertSelectKey(board);
+
+		log.info(board);
+	}*/
+	
+	
+	/*@Test
+	public void testRead() {
+		
+		BoardVO board = mapper.read(5L);
+		log.info(board);
+	}*/
+	
+	/*@Test
+	public void testdelete() {
+		
+		log.info("DELETE COUNT : " + mapper.delete(3L));
+		
+	}*/
+	
+	/*
+	@Test
+	public void testupdate() {
+		
+		BoardVO board = new BoardVO();
+		board.setBno(15);
+		board.setTitle("수정된 제목");
+		board.setContent("수정된 내용");
+		board.setWriter("UESR UPDATE");
+		
+		int count = mapper.update(board);
+		log.info("UPDATE COUNT : " +count);
+	}
+	
+	
+	*/
+	
+/*	@Test
+	public void testPaging() {
+		Criteria cri = new Criteria();
+		
+		cri.setPageNum(2);
+		cri.setAmount(10);
+		
+		List<BoardVO> list = mapper.getListWithPaging(cri);
+		
+		list.forEach(board -> log.info(board.getBno()));
+		
+	}
+	
+	*/
+	
+	
+	@Test
+	public void testSearch() {
+		
+		Criteria cri = new Criteria();
+		cri.setKeyword("paka");
+		cri.setType("");
+		
+		List<BoardVO> list = mapper.getListWithPaging(cri);
+		
+		list.forEach(board -> log.info(board));
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+}
